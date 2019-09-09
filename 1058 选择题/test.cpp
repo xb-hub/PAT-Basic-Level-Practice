ba4d0f1faa835ce0775dcc0fbd9e1b375ee732a6
xb <934814993@qq.com>
2019-09-10 00:23:26 +0800

    PAT

--- 1058 选择题/test.cpp
+++ 1058 选择题/test.cpp
@@ -9,59 +9,94 @@ struct Choose
     int score;
     int nums;
     string answer;
-};
+} choose[101];
 
-
+/**
+ * 测试点0输入n, m后有空格
+ */
 int main()
 {
-    Choose choose[101];
+    string n_m;
     int n, m;
-    cin >> n >> m;
-    getchar();
-    string s, stu;
-    vector<int> grade(m), num(m);
+    // cin >> n >> m;
+    getline(cin, n_m);
+    int b = 0;
+    for(int i = 0; i < n_m.size(); i++)
+    {
+        if(n_m[i] == ' ')
+        {
+            b = i;
+            break;
+        }
+    }
+    string s_n = n_m.substr(0, b);
+    string s_m;
+    if(n_m[n_m.size() - 1] == ' ')   s_m = n_m.substr(b + 1, n_m.size() - b - 2);
+    else    s_m = n_m.substr(b + 1, n_m.size() - b - 1);
+    n = atoi(s_n.c_str());
+    m = atoi(s_m.c_str());
+    // cout << n << " " << m;
+    int error[101] = {0};
+    string s;
     for(int i = 0; i < m; i++)
     {
         getline(cin, s);
-        // choose[i].score = stoi(s.substr(0, 1));
-        // choose[i].nums = stoi(s.substr(4, 1));
         choose[i].score = s[0] - '0';
         choose[i].nums = s[4] - '0';
         choose[i].answer = s.substr(6, s.size() - 6);
     }
-    // cout << choose[0].score << "  " << choose[0].nums << "  " << choose[0].answer << endl;
-    int score = 0, k = 0, site = 1;
     for(int i = 0; i < n; i++)
     {
+        int score = 0, site = 1;
         getline(cin, s);
         for(int j = 0; j < m; j++)
         {
             int num = s[site] - '0';
-            if(num != choose[j].nums)
-            {
-                // cout << j << "  " << k << "  " << choose[0].nums << endl;
-                // cout << "ERROR" << endl;
-                if(j != m - 1)
-                {
-                    site += (num * 2 + 4);
-                    continue;
-                }
-                else
-                {
-                    site = 1;
-                    continue;
-                }
-                
-            }
+            // if(num != choose[j].nums)
+            // {
+            //     if(j != m - 1)
+            //     {
+            //         error[j]++;
+            //         site += (num * 2 + 4);
+            //         continue;
+            //     }
+            //     else
+            //     {
+            //         error[j]++;
+            //         site = 1;
+            //         continue;
+            //     }   
+            // }
             string c;
-            for(int l = 0; l < num; l++)
+            c = s.substr(site + 2, num * 2 - 1);
+            // cout << c << " " << choose[j].answer << endl;
+            if(!c.compare(choose[j].answer))   score += choose[j].score;
+            else    error[j]++;
+            if(j != m - 1)  site += (num * 2 + 4);
+        }
+        printf("%d\n", score);
+    }
+    int max_error = 0;
+    for(int i = 0; i < m; i++)
+    {
+        if(error[i] > max_error)
+        {
+            max_error = error[i];
+        }
+    }
+    if(max_error == 0)
+    {
+        printf("Too simple");
+    }
+    else
+    {
+        printf("%d", max_error);
+        for(int i = 0; i < m; i++)
+        {
+            if(error[i] == max_error)
             {
-                c = s.substr(site + 2, num * 2 - 1);
-                cout << c << choose[j].answer << endl;
-                if(!c.compare(choose[j].answer))   score += choose[i].score;
+                printf(" %d", i + 1);
             }
         }
-        site = 1;
-        cout << score << endl;
     }
 }
---
+++ 1059 C语言竞赛/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    map<int, string> stu;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> stu[i];
+    }
+    int k;
+    cin >> k;
+    for(int i = 0; i < k; i++)
+    {
+        
+    }
+}
